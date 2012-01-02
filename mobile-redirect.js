4d5e67a457c6fbf7276d331048a60ddb50c5e9a3
Austin Catcher <austin@codefuze.com>
2012-01-02 15:39:12 -0600

    Added more UAs

--- mobile-redirect.js
+++ mobile-redirect.js
@@ -1,6 +1,6 @@
 function mobile_detect(mobile,tablet,mobile_redirect,tablet_redirect,debug) {
-	var ismobile = (/iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile/i.test(navigator.userAgent.toLowerCase()));
-	var istablet = (/ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()));
+	var ismobile = (/iphone|ipod|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile/i.test(navigator.userAgent.toLowerCase()));
+	var istablet = (/ipad|android 3|sch-i800|playbook|tablet|kindle|gt-p1000|sgh-t849|shw-m180s|a510|a511|a100|dell streak|silk/i.test(navigator.userAgent.toLowerCase()));
 
 	if (debug == true) {
 		alert(navigator.userAgent);
